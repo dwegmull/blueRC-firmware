0d47a3611f767051daa15e3b216c5abc008f1210
David Wegmuller <david@wegmuller.org>
2013-02-08 09:04:52 -0800

    Added BT module initialisation (speed and name). Still a bit buggy...

--- Blue_RC.ino
+++ Blue_RC.ino
@@ -77,31 +77,32 @@
 #define  REG_EEPROM_VALID     6
 #define  REG_FEATURES         7
 #define  REG_LO_THROTTLE      8
-#define  REG_HI_THROTTLE      9
-#define  REG_LO_REVERSE1      10
-#define  REG_MI_REVERSE1      11
-#define  REG_HI_REVERSE1      12
-#define  REG_LO_REVERSE2      13
-#define  REG_MI_REVERSE2      14
-#define  REG_HI_REVERSE2      15
-#define  REG_LO_DRAIN1        16
-#define  REG_HI_DRAIN1        17
-#define  REG_LO_DRAIN2        18
-#define  REG_HI_DRAIN2        19
-#define  REG_LO_WHISTLE       20
-#define  REG_HI_WHISTLE       21
-#define  REG_DISC_TIMEOUT     22
-#define  REG_VERSION_H        23
-#define  REG_VERSION_F        24
-#define  REG_VERSION_P        25
-#define  REG_SAFE_THROTTLE    26
-#define  REG_SAFE_REVERSE1    27
-#define  REG_SAFE_REVERSE2    28
-#define  REG_SAFE_DRAIN1      29
-#define  REG_SAFE_DRAIN2      30
-#define  REG_SAFE_WHISTLE     31
-#define  REG_DESC_SIZE        32
-#define  REG_DESCRIPTION      33
+#define  REG_MI_THROTTLE      9
+#define  REG_HI_THROTTLE      10
+#define  REG_LO_REVERSE1      11
+#define  REG_MI_REVERSE1      12
+#define  REG_HI_REVERSE1      13
+#define  REG_LO_REVERSE2      14
+#define  REG_MI_REVERSE2      15
+#define  REG_HI_REVERSE2      16
+#define  REG_LO_DRAIN1        17
+#define  REG_HI_DRAIN1        18
+#define  REG_LO_DRAIN2        19
+#define  REG_HI_DRAIN2        20
+#define  REG_LO_WHISTLE       21
+#define  REG_HI_WHISTLE       22
+#define  REG_DISC_TIMEOUT     23
+#define  REG_VERSION_H        24
+#define  REG_VERSION_F        25
+#define  REG_VERSION_P        26
+#define  REG_SAFE_THROTTLE    27
+#define  REG_SAFE_REVERSE1    28
+#define  REG_SAFE_REVERSE2    29
+#define  REG_SAFE_DRAIN1      30
+#define  REG_SAFE_DRAIN2      31
+#define  REG_SAFE_WHISTLE     32
+#define  REG_DESC_SIZE        33
+#define  REG_DESCRIPTION      34
 #define  MAX_REGISTERS        255
 #define  START_OF_EEPROM      REG_EEPROM_VALID
 #define  REG_SAFE_BASE        REG_SAFE_THROTTLE
@@ -117,7 +118,7 @@ char servos[NUMBER_OF_SERVOS];
 char serialBuff[SERIAL_BUFF_SIZE];
 volatile unsigned char timeout;
 char timeCt;
-char dataSize;
+int dataSize;
 
 char servoState;
 union w2b
@@ -200,7 +201,7 @@ void char2ASCII(char c, char *a)
 // Returns 0 if a packet arrived, 1 if timeout, 2 if error
 char receivePacket(void)
 {
-  while(timeout > 1)
+  while(timeout != 1)
   {
 
     if(UCSR0A & (1 << RXC0))
@@ -212,7 +213,7 @@ char receivePacket(void)
         
         // found a start of packet: read the rest of it
         dataSize = 0;
-        while(timeout > 1)
+        while(timeout != 1)
         {
           // Receiving a packet
           if(UCSR0A & (1 << RXC0))
@@ -250,8 +251,84 @@ void txString(char* s)
   }
 }
 
+// Wait with timeout for an incoming string on the UART.
+// Check if the string matches the one given as an argument.
+// Return 0 if a matching string is received.
+// Return 1 in case of a timeout.
+// Return 2 if the string does not match.
+char getString(char* s)
+{
+  while(*s != 0)
+  {
+    int timeout = 0;
+    while(0 == (UCSR0A & (1 << RXC0)))
+    {
+      timeout++;
+      if(timeout > 20000)
+      {
+        // Timeout.
+        return 1;
+      }
+    }
+    // A character arrived: check if it's the one we expect
+    if(UDR0 != *s)
+    {
+      // String does not match!
+      return 2;
+    }
+    s++;
+  }
+  // A matching string was received!
+  return 0;
+}
+
+
+void stop_UART()
+{
+  // Wait to make sure the transmission is done before shutting down the UART.
+  while(0 == (UCSR0A & TXC0));
+// Disable the UART
+  UCSR0B = 0;
+  UCSR0A = 0;
+  UCSR0C = 0;
+}
+
+void start_UART_100K()
+{
+// Setup UART for 115200 bits/S
+  UBRR0H = 0;
+  UBRR0L = 8;
+  
+  // Enable receiver and transmitter
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  UCSR0A = (1<<U2X0);
+  
+  // Set frame format: 8 data, 1 stop bit
+  UCSR0C = (3<<UCSZ00);
+}
+
+void start_UART_50K()
+{
+// Setup the UART for 57600 bits/S
+  UBRR0H = 0;
+  UBRR0L = 16;
+ // Enable receiver and transmitter
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  UCSR0A = (1<<U2X0);
+  UCSR0C = (3<<UCSZ00);
+}
+
+void active_wait(int timeCt)
+{
+  for(timeCt = 0; timeCt < 10000; timeCt++)
+  {
+   PINB |= 0x04;
+  }  
+}
+
 void setup()
 {
+  unsigned int timeCt;
   // All servo signals are output low
   THROTTLE_DIR |= THROTTLE_MASK;
   THROTTLE_PORT &= ~THROTTLE_MASK;
@@ -285,14 +362,96 @@ void setup()
   
   // disable timer 0 to prevent jitter on the PWMs
   TCCR0B = 0;
-  // Setup the UART for 57600 bits/S
-  UBRR0H = 0;
-  UBRR0L = 16;
-  // Enable receiver and transmitter
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-  UCSR0A = (1<<U2X0);
-  // Set frame format: 8 data, 1 stop bit
-  UCSR0C = (3<<UCSZ00);
+  
+  // In most cases, the BT module is already setup to run at 50Kbits/S
+  start_UART_50K();
+  
+  // Wait to make sure the Bluetooth module is done booting
+  // It continuously toggles a GPIO once it is ready.
+  while(0 == (PINB & 0x02));
+  while(PINB & 0x02);
+  
+  // It is now safe to talk to the BT module.
+  txString("$$$");
+  if(0 != getString("CMD\r\n"))
+  {
+    // Failed to communicate with the BT module at 50K. Try at 100K
+    stop_UART();
+    start_UART_100K();
+    txString("$$$");
+    // Wait a bit between commands
+    DDRB |= 0x04;
+    active_wait(20000);
+
+    // Set the BT module bit bit rate to 57600 bits/s
+    txString("U,57.6,N\r");
+    stop_UART();
+    start_UART_50K();
+    txString("$$$");
+    while(0 != getString("CMD\r\n"))
+    {
+      // Try again
+      txString("$$$");
+    }
+    // Permanently set the BT module UART speed to 50K
+    txString("SU,57\r");
+    while(0 != getString("AOK\r\n"));
+  }
+  // in command mode at 50K
+  if(0x42 == EEPROM.read(REG_EEPROM_VALID - START_OF_EEPROM))
+  {
+    int nameSize = EEPROM.read(REG_DESC_SIZE - START_OF_EEPROM);
+    if(nameSize > 0)
+    {
+      // The locomotive name stored in the EEPROM is valid
+      int cnt;
+      if(nameSize > 18)
+      {
+        nameSize = 18;
+      }
+      for(cnt = 0; cnt < nameSize; cnt++)
+      {
+        unsigned char c;
+        c = EEPROM.read(cnt + REG_DESCRIPTION - START_OF_EEPROM);
+        if(c > 122)
+        {
+          c = 'X';
+        }
+        serialBuff[cnt] = c; 
+      }
+      serialBuff[cnt++] = '\r'; // The BT module returns a CR+ LF at the end of its messages
+      serialBuff[cnt++] = '\n';
+      serialBuff[cnt] = 0; // zero termination needed by getString.
+      // Ask the BT module for its name
+      txString("GN\r");
+      // Check its answer against the name stored in EEPROM.
+      cnt = getString(serialBuff);
+      if(2 == cnt)
+      {
+        // Names don't match: program the new one into the BT module
+        active_wait(10000);
+        txString("SN,");
+        serialBuff[nameSize + 1] = 0;
+        txString(serialBuff);
+        while(0 != getString("AOK\r\n"));
+        // Reset the BT module to use the new name.
+        DDRC |= 0x08;
+        PORTC &= ~0x08;
+        active_wait(10000);
+        PORTC |= 0x08;
+      }
+      else
+      {
+        // Get out of command mode
+        txString("---\r");
+      }
+    }
+  }
+  else
+  {
+    // Get out of command mode
+    txString("---\r");
+  }
   sei();
 }
 
@@ -311,6 +470,7 @@ void loop()
   if(0 == receivePacket())
   {
     char regBase = 0;
+    char stringOffset = 0;
   
     // We have a valid packet: reset the keepalive timer and let's process the packet
     if(0x42 == EEPROM.read(REG_EEPROM_VALID - START_OF_EEPROM))
@@ -340,16 +500,27 @@ void loop()
             if(loopCt < NUMBER_OF_SERVOS)
             {
               // set servo
-              
               servos[loopCt] = ASCII2char(serialBuff[bufferOffset], serialBuff[bufferOffset + 1]);
             }
             else
             {
-              if((loopCt >= START_OF_EEPROM) && (loopCt <= MAX_REGISTERS))
+              if((loopCt >= START_OF_EEPROM) && (loopCt <= REG_DESC_SIZE))
               {
                 // write to an EEPROM register
                 EEPROM.write(loopCt - START_OF_EEPROM, ASCII2char(serialBuff[bufferOffset], serialBuff[bufferOffset + 1]));
               }
+              else
+              {
+                if(loopCt == REG_DESCRIPTION)
+                {
+                  stringOffset = bufferOffset;
+                }
+                if(loopCt <= MAX_REGISTERS)
+                {
+                  EEPROM.write(loopCt - START_OF_EEPROM, serialBuff[stringOffset]);
+                  stringOffset++;
+                }
+              }            
             }
             bufferOffset += 2;
           }
@@ -364,25 +535,42 @@ void loop()
       case COM_QUERY:
         if(dataSize)
         {
-          char loopCt, bufferOffset;
+          int loopCt, bufferOffset;
+          char tempBuff[3];
+          tempBuff[2] = 0;
 
           bufferOffset = TX_DATA_START;
-          for(loopCt = regBase; loopCt < (regBase + dataSize); loopCt++)
+          for(loopCt = 0; loopCt < dataSize; loopCt++)
           {
-            if(loopCt < NUMBER_OF_SERVOS)
+            if(regBase < NUMBER_OF_SERVOS)
             {
               // read servo
-              char2ASCII(servos[loopCt], &serialBuff[bufferOffset]);
+              char2ASCII(servos[regBase], &serialBuff[bufferOffset]);
+              bufferOffset += 2;
             }
             else
             {
-              if((loopCt >= START_OF_EEPROM) && (loopCt <= MAX_REGISTERS))
+              if((regBase >= START_OF_EEPROM) && (regBase <= REG_DESC_SIZE))
               {
                 // read from an EEPROM register
-                char2ASCII(EEPROM.read(loopCt - START_OF_EEPROM), &serialBuff[bufferOffset]);
+                char2ASCII(EEPROM.read(regBase - START_OF_EEPROM), &serialBuff[bufferOffset]);
+                bufferOffset += 2;
+              }
+              else
+              {
+                if(loopCt <= MAX_REGISTERS)
+                {
+/*                  char2ASCII(stringOffset, tempBuff);
+                  txString(tempBuff);
+                  char2ASCII(loopCt, tempBuff);
+                  txString(tempBuff);
+                  */
+                  serialBuff[bufferOffset] = EEPROM.read(regBase - START_OF_EEPROM);
+                  bufferOffset++;
+                }
               }
             }
-            bufferOffset += 2;
+            regBase++;
           }
           serialBuff[TX_RESPONSE] = RESP;
           serialBuff[bufferOffset] = SERIAL_END_TX;
